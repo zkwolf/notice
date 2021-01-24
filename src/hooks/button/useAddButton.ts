7a3b0df1cf95094ba087468ee707eac03e163fcd
zkwolf <chenhao5866@gmail.com>
2021-01-24 23:41:38 +0800

    feat: add button

--- src/hooks/button/useAddButton.ts
+++ src/hooks/button/useAddButton.ts
@@ -1,11 +1,14 @@
 const useAddButton = () => {
-  return {
+  const props = {
+    size: 'large',
+    shape: 'circle',
     style: {
       position: 'fixed',
       bottom: '30px',
       right: '30px',
     },
   };
+  return props;
 };
 
 export default useAddButton;
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,13 +1,35 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import useAddButton from '/@/hooks/button/useAddButton';
+import PlusOutlined from '@ant-design/icons-vue/PlusOutlined';
 
-const bottom = ref(10);
+const buttonProps = useAddButton();
+
+const onMenuClick = ({ key }: any) => {
+  console.log(key);
+};
+
+const menuList = [
+  { label: '生活', value: 'life' },
+  { label: '金钱', value: 'money' },
+];
 </script>
 
 <template>
   <a-layout>
     <a-card></a-card>
-    <a-card></a-card>
-    <a-button type="primary">Affix bottom</a-button>
+    <a-dropdown :trigger="['click']" placement="topCenter">
+      <a-button type="primary" v-bind="buttonProps">
+        <template #icon>
+          <plus-outlined />
+        </template>
+      </a-button>
+      <template #overlay>
+        <a-menu @click="onMenuClick">
+          <template v-for="item in menuList" :key="item.value">
+            <a-menu-item>{{ item.label }}</a-menu-item>
+          </template>
+        </a-menu>
+      </template>
+    </a-dropdown>
   </a-layout>
 </template>
